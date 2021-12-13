daf945fed116cc29aa6eef55214c187e8fc9c691
gsaleon <garin.s.a@gmail.com>
2021-12-14 03:54:29 +0700

    Add structure Handle 3

--- src/Services/ParseCommandLine.hs
+++ src/Services/ParseCommandLine.hs
@@ -1,48 +1,49 @@
 module Services.ParseCommandLine where
 
-import Data.List (isPrefixOf)
 import Data.Char (isDigit)
 
-str = "-r 5 --service=telegramm --loglevel=warning"
-{--
-parseLine :: String -> [(String,String)]
-parseLine str = if help /= []
-                  then help
-                  else repeat
---}
+main = do
+  str <- getLine
+  putStrLn ("str - " ++ show str)
+  putStrLn (show $ parseLine' str arg)
+  putStrLn (show $ parseLine str arg)
 
-main =
-  print (help, repeat', service, loglevel)
+arg :: [(String,String)]
+arg = [("-h","--help")
+      ,("-s tel","--service=telegramm")
+      ,("-s vc","--service=vkontakte")
+      ,("-l d","--loglevel=debug")
+      ,("-l i","--loglevel=info")
+      ,("-l w","--loglevel=warning")
+      ,("-l e","--loglevel=error")
+      ,("-r ","--repeat=")      
+      ]
 
-help = parseLineHelp str
+parseLine :: String -> [(String,String)]-> [String]
+parseLine str arg = let parse = map snd $ parseLine' str arg in
+  if (null parse) && (not $ null str)
+    then ["errorParsing"]
+    else if head parse == "help"
+           then ["help"]
+           else if (null $ findNumber str) && (findStr (concat parse) "repeat=")
+                  then ["errorParsing"]
+                  else if (length parse == 1)
+                         then [head parse ++ findNumber str]
+                         else init parse ++ [last parse ++ findNumber str]
 
-parseLineHelp :: String  -> [(String,String)]
-parseLineHelp str = if "-h" `isPrefixOf` str || "--help" `isPrefixOf` str
-                      then [("help","")] else []
+parseLine' :: String -> [(String,String)] -> [(Bool,String)]
+parseLine' str arg =
+  filter (\x -> fst x) $
+    map (\x -> ((findStr str (fst x))
+             || (findStr str (snd x)),(tail . tail $ snd x))) arg
 
-repeat' = parseLineRepeat str
+findStr :: String -> String -> Bool
+findStr [] _ = False
+findStr s@(x:xs) pat
+  | pat == (take (length pat) s) = True
+  | otherwise                    = findStr xs pat
 
-parseLineRepeat :: String -> [(String,String)]
-parseLineRepeat str = if "-r " `isPrefixOf` str || "--repeat=" `isPrefixOf` str
-                        then f str else []
-  where
-    f [] = []
-    f (x:xs) = if (isDigit x) then [("repeat","x")] else f xs
+findNumber :: String -> String
+findNumber [] = []
+findNumber (x:xs) = if (isDigit x) then [x] else findNumber xs
 
-service = parseLineService str
-
-parseLineService :: String -> [(String,String)]
-parseLineService str
-  | ("-s tel" `isPrefixOf` str || "--service=telegramm" `isPrefixOf` str) = [("service","telegramm")]
-  | ("-s vc" `isPrefixOf` str  || "--service=vkontakte" `isPrefixOf` str) = [("service","vkontakte")]
-  | otherwise                                                             = []
-
-loglevel = parseLineLogLevel str
-
-parseLineLogLevel :: String -> [(String,String)]
-parseLineLogLevel str
-  | ("-l d" `isPrefixOf` str || "--loglevel=debug" `isPrefixOf` str)   = [("logLevel","debug")]
-  | ("-l i" `isPrefixOf` str || "--loglevel=info" `isPrefixOf` str)    = [("logLevel","info")]
-  | ("-l w" `isPrefixOf` str || "--loglevel=warning" `isPrefixOf` str) = [("logLevel","warning")]
-  | ("-l e" `isPrefixOf` str || "--loglevel=error" `isPrefixOf` str)   = [("logLevel","error")]
-  | otherwise                                                          = []
